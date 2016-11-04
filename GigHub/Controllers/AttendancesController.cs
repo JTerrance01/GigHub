068f0991c653d9b439317abd3c47f613d3028323
Terrance Jones <jterrance0257@gmail.com>
2016-11-03 20:40:06 -0500

    Implement Index Going button

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,42 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            //  NOTE: _context Initialized in the constructor
+            _context = new ApplicationDbContext();
+        }
+
+        //  NOTE: ASP.NET WebAPI does not look for a scatter perameter like an interger
+        //  CONTINUED - in the request body. It expects them to be in the URL
+        //  NOTE: Decarate the gigId parameter with the [FromBody] Attribute
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("The attendance already exists.");
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ GigHub/Models/Attendance.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    public class Attendance
+    {
+        //  NOTE: Navagation properties
+        public Gig Gig { get; set; }
+        public ApplicationUser Attendee { get; set; }
+
+        //  NOTE: Foreign key properties. AttendeeId reflects the ApplicationUserId
+        //  NOTE: Data Annotations Key and Column are used to create a COMPOSITE KEY
+        //  NOTE: Column - specifies the order of the key
+        [Key]
+        [Column(Order = 1)]
+        public int GigId { get; set; }
+
+        //  NOTE: The Id property in the ApplicationUser class is a string
+        //  CONT. AttendeeId will be mapped to the ApplicationUserId property
+        [Key]
+        [Column(Order = 2)]
+        public string AttendeeId { get; set; }
+    }
+}
