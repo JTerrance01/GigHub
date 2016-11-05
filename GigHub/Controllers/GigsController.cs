4ca86b411d46b77efcf77ff3f7e415a2a4e5a753
Terrance Jones <jterrance0257@gmail.com>
2016-11-05 00:29:40 -0500

    Implemented the Update operation

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -62,10 +62,34 @@ namespace GigHub.Controllers
         {
             var dropdownListOfGenres = new GigFormViewModel
             {
-                Genres = _context.Genres.ToList()
+                Genres = _context.Genres.ToList(),
+                Heading = "Add a Gig",
             };
 
-            return View(dropdownListOfGenres);
+            return View("GigForm", dropdownListOfGenres);
+        }
+
+        [Authorize]
+        public ActionResult Edit(int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            //  Note: The ArtistId is the id of the currently logged in user
+            var gig = _context.Gigs
+                .Single(g => g.Id == gigId && g.ArtistId == userId);
+
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList(),
+                Id = gig.Id,
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Venue = gig.Venue,
+                Heading = "Edit a Gig"
+            };
+
+            return View("GigForm", viewModel);
         }
 
         [Authorize]
@@ -85,7 +109,7 @@ namespace GigHub.Controllers
             if (!ModelState.IsValid)
             {
                 viewModel.Genres = _context.Genres.ToList();
-                return View("Create", viewModel);
+                return View("GigForm", viewModel);
             }
 
             var gig = new Gig
@@ -97,6 +121,32 @@ namespace GigHub.Controllers
             };
 
             _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken] // NOTE: (CSRF) used to combat
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            var userId = User.Identity.GetUserId();
+
+            //  NOTE: Server-side Validation - ModelState.IsValid return Create viewModel
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var gig = _context.Gigs
+                .Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
             _context.SaveChanges();
 
             return RedirectToAction("Mine", "Gigs");
